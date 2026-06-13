ea7968db1bb5f7e7377fe55ec9dd7db303e4e45d
maprunty <maprunty@student.42heilbronn.de>
2026-06-13 10:22:20 +0200

    refactor: main and .toml to bring in line with current focus

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+#!/usr/bin/env python3
+# *************************************************************************** #
+#                                                                             #
+#                                                        :::      ::::::::    #
+#    main.py                                           :+:      :+:    :+:    #
+#                                                    +:+ +:+         +:+      #
+#    By: maprunty <maprunty@student.42heilbronn.d  +#+  +:+       +#+         #
+#                                                +#+#+#+#+#+   +#+            #
+#    Created: 2026/06/11 15:24:56 by maprunty         #+#    #+#              #
+#    Updated: 2026/06/13 05:56:47 by maprunty        ###   ########.fr        #
+#                                                                             #
+# *************************************************************************** #
+from display import TuiDisplay
+from parser import Parser
+from simulation import Simulation
+
+
 def main():
     print("Hello from fly-in!")
+    dm = Parser().parse_file("maps/easy/02_simple_fork.txt")
+    sim = Simulation(drone_map=dm)
+    disp = TuiDisplay(dm)
+    disp.render(sim)
 
 
 if __name__ == "__main__":
